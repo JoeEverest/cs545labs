8e922a5ccd6a3831b989a5840d482ad327294168
JoeEverest <everestjoseph11@yahoo.com>
2025-02-25 13:33:12 -0600

    initial commit

---
+++ src/main/java/edu/miu/cs545labs/Cs545labsApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs545labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs545labsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs545labsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/cs545labs/Cs545labsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs545labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs545labsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
